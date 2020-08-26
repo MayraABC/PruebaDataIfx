0d475988b5e135f77756030200b4157be9b89c89
MayralejandraABC <mayrabarrioscastillo@hotmail.com>
2020-08-26 18:17:59 -0500

    Componente professors, listado de estos.

--- src/app/components/professors/professors.component.ts
+++ src/app/components/professors/professors.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonService } from '../../services/person.service';
+import { Person } from 'src/app/interfaces/person';
 
 @Component({
   selector: 'app-professors',
   templateUrl: './professors.component.html',
-  styleUrls: ['./professors.component.scss']
+  styleUrls: ['./professors.component.scss'],
 })
 export class ProfessorsComponent implements OnInit {
-
-  constructor() { }
+  listProfessors: Person[] = [];
+  constructor(private personService: PersonService) {}
 
   ngOnInit(): void {
+    this.getProfessors();
   }
 
+  getProfessors(): void {
+    this.personService.getProfessors().subscribe((response) => {
+      this.listProfessors = [];
+      for (const p of response) {
+        this.listProfessors.push({
+          name: p.name,
+          patronus: p.patronus,
+          age: this.personService.calculateAge(p.yearOfBirth),
+          image: p.image,
+        });
+      }
+    });
+  }
 }
